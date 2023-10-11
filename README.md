d11d8f85d9275e8611ca79795eb79662b7d5e620
hasinaiaina <aina@wau.solutions>
2023-10-11 11:51:11 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,89 @@
-# rewards-smart-contract
+# Voxiberate Rewards Contract
+
 This is the Voxiberate Rewards Contract, a smart contract written in C# for the Neo blockchain platform. It allows for the management and distribution of rewards to citizens.
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Prerequisites](#prerequisites)
+- [Features](#features)
+- [Getting Started](#getting-started)
+- [Usage](#usage)
+- [Events](#events)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Introduction
+
+The Voxiberate Rewards Contract is designed to facilitate the distribution of rewards to citizens. It provides functions for storing rewards, refunding rewards, locking rewards, submitting citizens, and distributing rewards.
+
+## Prerequisites
+
+[.NET Core 6.0 SDK](https://dotnet.microsoft.com/en-us/download/dotnet/6.0)
+
+Please refer here for [Neo Blockchain Toolkit for .NET Quickstart](https://github.com/neo-project/neo-blockchain-toolkit/blob/master/quickstart.md)
+
+You will need to copy the smart contract under the neo-express toolkit src.
+
+Useful link [tutorial](https://developers.neo.org/tutorials/2021/05/27/getting-started-with-the-neo-blockchain-toolkit)
+
+## Features
+
+- Store rewards with facilitator, token, amount, and state information
+- Refund rewards to facilitators
+- Lock rewards to prevent further modifications
+- Submit citizens for rewards
+- Distribute rewards to top 5 citizens and full participation citizens
+
+## Getting Started
+
+To use the Voxiberate Rewards Contract, you will need the following:
+
+- Neo blockchain platform
+- Neo Smart Contract development environment
+- Voxiberate Rewards Contract source code
+
+## Usage
+
+1. Build the smart contract 
+```
+$ dotnet build
+
+MSBuild version 17.3.2+561848881 for .NET
+  Determining projects to restore...
+  All projects are up-to-date for restore.
+  RewardsContract -> /home/hasinapic/Workspaces/WAUIO/VOXIBERATE/rewards-smart-contract/src/bin/sc/RewardsContract.nef
+  RewardsContract -> /home/hasinapic/Workspaces/WAUIO/VOXIBERATE/rewards-smart-contract/src/bin/Debug/net6.0/RewardsContract.dll
+
+Build succeeded.
+    0 Warning(s)
+    0 Error(s)
+
+Time Elapsed 00:00:03.17
+```
+2. Deploy the Voxiberate Rewards Contract on the Neo blockchain platform.
+
+[Testnet deployed smart contract](https://testnet.neotube.io/contract/0xc6e193931705ab8524da4280c9000bc57641be8c)
+
+3. Call the various functions provided by the contract to manage and distribute rewards.
+4. Listen to the events emitted by the contract to track reward-related activities.
+
+## Events
+
+The Voxiberate Rewards Contract emits the following events:
+
+- `RewardStored`: Triggered when a reward is stored with facilitator, token, and amount information.
+- `RewardRefunded`: Triggered when a reward is refunded to a facilitator.
+- `CitizenSubmitted`: Triggered when citizens are submitted for a reward.
+- `RewardDistributed`: Triggered when rewards are distributed to citizens.
+- `AdminAdded`: Triggered when a new admin is added to the contract.
+- `AdminRemoved`: Triggered when an admin is removed from the contract.
+- `RewardLocked`: Triggered when a reward is locked.
+
+## Contributing
+
+Contributions to the Voxiberate Rewards Contract are welcome! If you find any issues or have suggestions for improvements, please open an issue or submit a pull request.
+
+## License
+
+The Voxiberate Rewards Contract is released under the [MIT License](LICENSE).
---
+++ src/RewardsContract.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.ComponentModel;
+using System.Numerics;
+
+using Neo;
+using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Attributes;
+using Neo.SmartContract.Framework.Native;
+using Neo.SmartContract.Framework.Services;
+
+namespace Voxiberate
+{
+    public enum RewardState : byte
+    {
+        Opened,
+        Locked,
+        Distributed,
+        Refunded
+    }
+    public class RewardsInfo
+    {
+        public UInt160 Facilitator = UInt160.Zero;
+        public UInt160 Token = UInt160.Zero;
+        public BigInteger Amount;
+        public UInt160[] Citizens;
+        public RewardState State;
+    }
+
+    [DisplayName("Voxiberate.RewardsContract")]
+    [ManifestExtra("Author", "Voxiberate")]
+    [ManifestExtra("Description", "Voxiberation rewards contract")]
+    [ContractPermission("*", "transfer")]
+    public class RewardsContract : SmartContract
+    {
+        const byte PREFIX_REWARDS = 0x00;
+        const byte PREFIX_ADMIN = 0x01;
+        const byte PREFIX_CITIZEN_MAP = 0x02;
+        const byte PREFIX_CONTRACT_OWNER = 0xFF;
+
+        private static Transaction Tx => (Transaction)Runtime.ScriptContainer;
+
+        [Safe]
+        public static UInt160 Owner() => (UInt160)Storage.Get(Storage.CurrentContext, new byte[] { PREFIX_CONTRACT_OWNER });
+
+        [Safe]
+        public static UInt160 Admin(UInt160 address) => (UInt160)new StorageMap(Storage.CurrentContext, PREFIX_ADMIN).Get(address);
+
+        public delegate void OnRewardStoredDelegate(string rewardKey, UInt160 facilitator, UInt160 token, BigInteger amount);
+
+        public delegate void OnRewardRefundedDelegate(string rewardKey, UInt160 facilitator, RewardState state);
+
+        public delegate void OnRewardCitizenDelegate(string rewardKey, UInt160 citizen, string type, bool status, BigInteger amount);
+
+        public delegate void OnCitizenSubmittedDelegate(string rewardKey);
+
+        public delegate void OnRewardDistributedDelegate(string rewardKey, Map<string, BigInteger> rewarded);
+
+        public delegate void OnAdminAddedDelegate(UInt160 admin);
+
+        public delegate void OnAdminRemovedDelegate(UInt160 admin);
+
+        public delegate void OnRewardLockedDelegate(string rewardKey);
+
+        [DisplayName("RewardStored")]
+        public static event OnRewardStoredDelegate OnRewardStored = default!;
+
+        [DisplayName("RewardRefunded")]
+        public static event OnRewardRefundedDelegate OnRewardRefunded = default!;
+
+        [DisplayName("CitizenSubmitted")]
+        public static event OnCitizenSubmittedDelegate OnCitizenSubmitted = default!;
+
+        [DisplayName("RewardDistributed")]
+        public static event OnRewardDistributedDelegate OnRewardDistributed = default!;
+
+        [DisplayName("AdminAdded")]
+        public static event OnAdminAddedDelegate OnAdminAdded = default!;
+
+        [DisplayName("AdminRemoved")]
+        public static event OnAdminRemovedDelegate OnAdminRemoved = default!;
+
+        [DisplayName("RewardLocked")]
+        public static event OnRewardLockedDelegate OnRewardLocked = default!;
+
+        [DisplayName("_deploy")]
+        public static void Deploy(object data, bool update)
+        {
+            if (update) return;
+
+            var key = new byte[] { PREFIX_CONTRACT_OWNER };
+            Storage.Put(Storage.CurrentContext, key, Tx.Sender);
+            new StorageMap(Storage.CurrentContext, PREFIX_ADMIN).Put(Tx.Sender, Tx.Sender);
+        }
+
+        public static void Update(ByteString nefFile, string manifest)
+        {
+            if (!Runtime.CheckWitness(Owner())) throw new Exception("Only owner can update the contract.");
+            ContractManagement.Update(nefFile, manifest, null);
+        }
+
+        public static void Destroy()
+        {
+            if (!Runtime.CheckWitness(Owner())) throw new Exception("Only owner can destroy the contract.");
+            ContractManagement.Destroy();
+        }
+
+        public static bool Transfer(UInt160 from, UInt160 to, BigInteger amount, object data)
+        {
+            return (bool)Contract.Call(Runtime.CallingScriptHash, "transfer", CallFlags.All, new object[] { from, to, amount, data });
+        }
+
+        public static void AddAdmin(UInt160 admin)
+        {
+            var owner = Owner();
+            if (!Runtime.CheckWitness(owner)) throw new Exception("Only owner can add new admin.");
+            new StorageMap(Storage.CurrentContext, PREFIX_ADMIN).Put(admin, admin);
+
+            OnAdminAdded(admin);
+        }
+
+        public static void RemoveAddmin(UInt160 admin)
+        {
+            if (!Runtime.CheckWitness(Owner())) throw new Exception("Only owner can remove an admin.");
+            new StorageMap(Storage.CurrentContext, PREFIX_ADMIN).Delete(admin);
+
+            OnAdminRemoved(admin);
+        }
+
+        public static void OnNEP17Payment(UInt160 from, BigInteger amount, string rewardKey)
+        {
+            if (from == null && Runtime.CallingScriptHash == GAS.Hash)
+            {
+                // When NEO balance changes, the contract receives GAS from the platform
+                // Ignore this payment for rewards purposes
+                return;
+            }
+
+            if (from == null) throw new ArgumentNullException(nameof(from));
+
+            if (rewardKey != null)
+            {
+                StoreRewards(from, amount, rewardKey);
+            }
+        }
+
+        [DisplayName("_storeRewards")]
+        static void StoreRewards(UInt160 facilitator, BigInteger amount, string rewardKey)
+        {
+            var token = Runtime.CallingScriptHash;
+            StorageMap rewardMap = new(Storage.CurrentContext, PREFIX_REWARDS);
+            var serializedReward = rewardMap.Get(rewardKey);
+            if (serializedReward != null) throw new Exception("Specified rewardKey already exists.");
+
+            var rewardInfo = new RewardsInfo();
+            rewardInfo.Facilitator = facilitator;
+            rewardInfo.Token = token;
+            rewardInfo.Amount = amount;
+            rewardInfo.State = RewardState.Opened;
+
+            rewardMap.Put(rewardKey, StdLib.Serialize(rewardInfo));
+
+            OnRewardStored(rewardKey, facilitator, token, amount);
+        }
+
+        public static void Refund(string rewardKey)
+        {
+            if (!Runtime.CheckWitness(Admin(Tx.Sender))) throw new Exception("Only a admin can refund a reward.");
+
+            StorageMap rewardMap = new(Storage.CurrentContext, PREFIX_REWARDS);
+            var serializedReward = rewardMap.Get(rewardKey);
+
+            if (serializedReward == null) throw new Exception("Rewards not found.");
+
+            var rewardInfo = (RewardsInfo)StdLib.Deserialize(serializedReward);
+
+            if (rewardInfo.State == RewardState.Refunded) throw new Exception("Rewards already refunded.");
+
+            if (rewardInfo.State == RewardState.Distributed) throw new Exception("Rewards already distributed.");
+
+            var isRefunded = (bool)Contract.Call(rewardInfo.Token, "transfer", CallFlags.All, new object[] { Runtime.ExecutingScriptHash, rewardInfo.Facilitator, rewardInfo.Amount, new byte[] { } });
+
+            if (!isRefunded) throw new Exception("Unable to refund rewards.");
+
+            rewardInfo.State = RewardState.Refunded;
+            rewardMap.Put(rewardKey, StdLib.Serialize(rewardInfo));
+
+            OnRewardRefunded(rewardKey, rewardInfo.Facilitator, rewardInfo.State);
+        }
+
+        public static void LockRewards(string rewardKey)
+        {
+            if (!Runtime.CheckWitness(Admin(Tx.Sender))) throw new Exception("Only an admin can lock a reward.");
+
+            StorageMap rewardMap = new(Storage.CurrentContext, PREFIX_REWARDS);
+            var serializedReward = rewardMap.Get(rewardKey);
+
+            if (serializedReward == null) throw new Exception("Invalid reward id.");
+            var rewardInfo = (RewardsInfo)StdLib.Deserialize(serializedReward);
+            rewardInfo.State = RewardState.Locked;
+            rewardMap.Put(rewardKey, StdLib.Serialize(rewardInfo));
+
+            OnRewardLocked(rewardKey);
+        }
+
+
+        public static void SubmitCitizens(string rewardKey, UInt160[] citizens)
+        {
+            if (!Runtime.CheckWitness(Admin(Tx.Sender))) throw new Exception("Only an admin can submit citizens.");
+
+            if (citizens == null || citizens.Length == 0) throw new Exception("The parameter citizens cannot be null or empty.");
+
+            StorageMap rewardMap = new(Storage.CurrentContext, PREFIX_REWARDS);
+            var serializedReward = rewardMap.Get(rewardKey);
+
+            if (serializedReward == null) throw new Exception("Invalid reward id.");
+
+            var rewardInfo = (RewardsInfo)StdLib.Deserialize(serializedReward);
+
+            if (rewardInfo.State != RewardState.Opened) throw new Exception("Cannot add more citizens");
+
+            StorageMap citizenMap = new(Storage.CurrentContext, PREFIX_CITIZEN_MAP);
+
+            foreach (var c in citizens)
+            {
+                var cHash = new List<object>();
+                cHash.Add(c);
+                cHash.Add(rewardKey);
+                var ck = CryptoLib.Sha256(StdLib.Serialize(cHash));
+                citizenMap.Put(ck, 1);
+            }
+
+            OnCitizenSubmitted(rewardKey);
+        }
+
+        public static void DistributeRewards(string rewardKey, UInt160[] top5citizens, UInt160[] fullParticitationCitizens)
+        {
+            if (!Runtime.CheckWitness(Admin(Tx.Sender))) throw new Exception("Only an admin can distribute rewards");
+
+            StorageMap rewardMap = new(Storage.CurrentContext, PREFIX_REWARDS);
+            var serializedReward = rewardMap.Get(rewardKey);
+
+            if (serializedReward == null) throw new Exception("Invalid reward key.");
+
+            var rewardInfo = (RewardsInfo)StdLib.Deserialize(serializedReward);
+
+            StorageMap citizenMap = new(Storage.CurrentContext, PREFIX_CITIZEN_MAP);
+
+            Map<string, BigInteger> rewardedMap = new();
+
+            foreach (var c5 in top5citizens)
+            {
+                var cHash = new List<object>();
+                cHash.Add(c5);
+                cHash.Add(rewardKey);
+                var ck = CryptoLib.Sha256(StdLib.Serialize(cHash));
+
+                var citizenStore = citizenMap.Get(ck);
+                if (citizenStore != null)
+                {
+                    var reward = (rewardInfo.Amount * 10) / 100;
+                    var isTransfered = (bool)Contract.Call(rewardInfo.Token, "transfer", CallFlags.All, new object[] { Runtime.ExecutingScriptHash, c5, reward, new byte[] { } });
+                    if (isTransfered) rewardedMap[c5] = reward;
+                }
+
+            }
+
+            var fullParticipationRewardPart = (rewardInfo.Amount * 50) / 100;
+            foreach (var cf in fullParticitationCitizens)
+            {
+                var cHash = new List<object>();
+                cHash.Add(cf);
+                cHash.Add(rewardKey);
+                var ck = CryptoLib.Sha256(StdLib.Serialize(cHash));
+
+                var citizenStore = citizenMap.Get(ck);
+                if (citizenStore != null)
+                {
+                    var reward = fullParticipationRewardPart / fullParticitationCitizens.Length;
+                    var isTransfered = (bool)Contract.Call(rewardInfo.Token, "transfer", CallFlags.All, new object[] { Runtime.ExecutingScriptHash, cf, reward, new byte[] { } });
+                    if (isTransfered) rewardedMap[cf] = reward;
+                }
+
+            }
+
+            rewardInfo.State = RewardState.Distributed;
+            rewardMap.Put(rewardKey, StdLib.Serialize(rewardInfo));
+
+            OnRewardDistributed(rewardKey, rewardedMap);
+
+        }
+    }
+}
